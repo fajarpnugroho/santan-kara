9e2af86697cb8094f33cf903724ea1eae5fb44a3
fajarpnugroho <fajar@kurio.co.id>
2015-06-21 11:36:00 +0700

    saved

--- app/src/main/java/euphoriadigital/karaoke/ui/RecordMovieFragment.java
+++ app/src/main/java/euphoriadigital/karaoke/ui/RecordMovieFragment.java
@@ -2,21 +2,26 @@ package euphoriadigital.karaoke.ui;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import butterknife.OnClick;
 import euphoriadigital.karaoke.R;
 
-public class RecordMovieFragment extends Fragment implements RecordMovieActionTaker {
+import static android.media.MediaPlayer.OnCompletionListener;
+import static android.media.MediaPlayer.OnPreparedListener;
+
+public class RecordMovieFragment extends Fragment implements RecordMovieActionTaker,
+        OnPreparedListener, OnCompletionListener {
 
     @InjectView(R.id.videoview) VideoView videoView;
 
@@ -50,20 +55,37 @@ public class RecordMovieFragment extends Fragment implements RecordMovieActionTa
     }
 
     @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        controller.recordMovie();
+    }
+
+    @Override
     public void onDestroyView() {
         ButterKnife.reset(this);
         super.onDestroyView();
-    }
-
-    @OnClick(R.id.button_start)
-    void onButtonStartClick() {
-        controller.recordMovie();
     }
 
     @Override
     public void showMovie(Uri videoUri) {
         Toast.makeText(getActivity(), "Record success", Toast.LENGTH_SHORT).show();
         videoView.setVideoURI(videoUri);
+        videoView.setOnPreparedListener(this);
+        videoView.setMediaController(new MediaController(getActivity()));
+        videoView.requestFocus();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        this.videoView.seekTo(0);
+        Toast.makeText(getActivity(), "Playback complete", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        this.videoView.setOnCompletionListener(this);
+        this.videoView.start();
+        Toast.makeText(getActivity(), "Video playback stard", Toast.LENGTH_SHORT).show();
     }
 
     public interface Controller {
---
+++ app/src/main/java/euphoriadigital/karaoke/util/CameraUtil.java
@@ -0,0 +1,87 @@
+package euphoriadigital.karaoke.util;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public final class CameraUtil {
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    public static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+
+    private static Uri fileUri;
+
+    public static void takePicture(Activity activity){
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+        // start the image capture Intent
+        activity.startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+
+    public static void takeVideo(Activity activity){
+        //create new Intent
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_VIDEO);  // create a file to save the video
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);  // set the image file name
+
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1); // set the video image quality to high
+
+        // start the Video Capture Intent
+        activity.startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
+    }
+
+
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "SantanKara");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+}
